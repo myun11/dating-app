9c3896075e18087fe4b4ddbab2a401c4182b674b
myun11 <yun.michael.ds@gmail.com>
2023-11-13 20:06:23 -0500

    tabs

---
+++ src/components/Tabs.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Panel from './Panel'
+import Favorited from './Favorited'
+
+const Tabs = () => {
+  return (
+    <div>
+        text
+
+    </div>
+  )
+}
+
+export default Tabs
